dfaed012c5a5301d206b0714748003393ea2ff62
Vital <Lihoy84@yandex.ru>
2020-09-14 13:36:31 +0300

    Updates

---
+++ precache-manifest.e5cbd62422e16a8ab143ec876b32927d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2529fec91f0e94d69822ee76c01fb4f8",
+    "url": "/todolist-react_p/index.html"
+  },
+  {
+    "revision": "83c2280116b2ff60070b",
+    "url": "/todolist-react_p/static/css/main.080239b7.chunk.css"
+  },
+  {
+    "revision": "26f48db85f446ffdaeb6",
+    "url": "/todolist-react_p/static/js/2.fd100086.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/todolist-react_p/static/js/2.fd100086.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83c2280116b2ff60070b",
+    "url": "/todolist-react_p/static/js/main.e820cae7.chunk.js"
+  },
+  {
+    "revision": "82704c81371ae1491beb",
+    "url": "/todolist-react_p/static/js/runtime-main.f1cdd1c1.js"
+  }
+]);
